5a2ffa226287de2e33701566ebc14ccd98576816
ShaySen <ssenha2@illinois.edu>
2020-09-08 03:49:57 +0000

    PROB 2 OUTPUTS

--- problem2.py
+++ problem2.py
@@ -1,7 +1,12 @@
-### PROBLEM 2
-
-def main():
-    print("hello world!")
-
-if __name__ == "__main__":
-    main()
+### PROBLEM 
+i=7
+j=2
+result= i**2 +3j-5
+print(result)
+#(44+3j)
+#when i was changed to i21, answer remained same: (44+3j)
+#When i21 was changed to 21i
+#ERROR MESSAGE: File "problem2.py", line 2
+#    21i=7
+#     ^
+#SyntaxError: invalid syntax 
